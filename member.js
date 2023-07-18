e01529c257f52768796cf58f87a83f19ac1c1070
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2023-07-18 09:19:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberTextValue = skillsMemberText + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    var skillsMemberTextValue = skillsMemberTextValue + " " + skillsMemberValue;
+    skills.value = skillsMemberTextValue;
+}
